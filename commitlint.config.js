71f3bff4872af6d17fe5ca1b19f6c8aa95da1834
mvc_ydb_admin <1609059419@qq.com>
2024-03-24 22:58:55 +0800

    chore: 📖 what?

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,6 @@
-const { getCommitlintConfig } = require('@x.render/render-lint');
-module.exports = getCommitlintConfig('common', {
-    // custom commitlint configuration
+const { getCommitlintConfig } = require("@x.render/render-lint");
+module.exports = getCommitlintConfig("common", {
+  rules: {
+    "header-max-length": [0, "always"],
+  },
 });
