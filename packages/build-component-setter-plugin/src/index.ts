9f03b4516476df99fbb7a0536043c5916aada5a1
render-ydb <yangdaobo.render@gmail.com>
2024-09-18 15:57:42 +0800

    feat: 🚀 新增编译组件设置器的plugin

--- packages/build-component-setter-plugin/src/index.ts
+++ packages/build-component-setter-plugin/src/index.ts
@@ -4,28 +4,25 @@ import {
   WebpackBuilderPluginClass,
   Json,
 } from '@x.render/render-builder';
-import StyleWebpackPlugin from '@x.render/style-webpack-plugin';
-import OptimizationWebpackPlugin from '@x.render/optimization-webpack-plugin';
-import ReactBabelWebpackPlugin from '@x.render/react-babel-webpack-plugin';
+import BuildReactComponentPlugin from '@x.render/build-react-component-webpack-plugin';
 import path = require('path');
 
 class BuildComponentSetterPlugin extends WebpackBuilderPluginClass {
   run(compiler: Compiler, config: ChainConfig, options: Json): ChainConfig {
     const { context } = compiler;
     const { rootDir, pkg } = context;
-    // 对setter进行样式处理
-    StyleWebpackPlugin.getConfig(compiler, config, {
+
+    BuildReactComponentPlugin.getConfig(compiler, config, {
       useRpx: false,
-      loaderOptions: {
-        miniCssExtractPluginOptions: {
-          filename: 'setter.css',
+      miniCssExtractPluginOptions: {
+        filename: 'setter.css',
+      },
+      cssLoaderOptions: {
+        modules: {
+          localIdentName: `${pkg.name}.${pkg.version}.[contenthash:8]`,
         },
       },
     });
-    // babel处理
-    ReactBabelWebpackPlugin.getConfig(compiler, config, {});
-    // 资源优化
-    OptimizationWebpackPlugin.getConfig(compiler, config, {});
 
     // 指定入口文件
     config.entryPoints.clear();
